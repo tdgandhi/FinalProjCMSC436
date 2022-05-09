01e3a81e493273b9d9ab201535ec2404e72beeef
zayd <zaydu98@gmail.com>
2022-05-08 22:06:47 -0400

    button w trail act

--- app/src/main/java/com/example/myapplication/Instructions.kt
+++ app/src/main/java/com/example/myapplication/Instructions.kt
@@ -1,11 +1,28 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Instructions : AppCompatActivity() {
+
+    companion object {
+
+    }
+
+    private lateinit var mButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mButton = findViewById(R.id.button)
+
+        mButton.setOnClickListener {
+            val mIntent = Intent(this, Trail::class.java)
+        }
     }
+
+
 }
---
+++ app/src/main/java/com/example/myapplication/Trail.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Trail {
+    
+}
